d3694ed52bf120f6e88a9185978a3e432c11bad8
Brett Jones <boobavon@gmail.com>
2012-03-04 11:53:33 -0500

    initial commit

---
+++ RingBuffer2d.c
@@ -0,0 +1,131 @@
+/*
+ 
+Copyright (c) 2012 R. Brett Jones
+
+Permission is hereby granted, free of charge, to any person obtaining a 
+copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the 
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included 
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "RingBuffer2d.h"
+
+int roundNextPowerof2(int inVal) {
+    int pow2 = 1;
+    while (inVal > pow2 && pow2 < (1 << 30) - 1) {
+        pow2 = pow2 << 1;
+    }
+    return pow2;  
+}
+
+// numRow will be rounded up to a power of 2
+RingBuffer2d * rb2dInit(int numCol, int numRow, size_t elemsize) {
+	RingBuffer2d *rb2d = malloc(sizeof(RingBuffer2d));
+	rb2d->rows = roundNextPowerof2(numRow);
+	rb2d->cols = numCol;
+	rb2d->eleSize = elemsize;
+	rb2d->writeIndex = rb2d->readIndex = 0;
+
+	rb2d->buffer = malloc(rb2d->rows * sizeof(void *));
+
+	int i;
+	for (i = 0; i < rb2d->rows; ++i)
+	{
+		rb2d->buffer[i] = calloc(rb2d->cols, rb2d->eleSize);
+	}
+	printf("Initialized rb2d. Row:%i Col:%i\n",rb2d->rows,rb2d->cols);
+
+	return rb2d;
+}
+
+void rb2dPush(RingBuffer2d *rb2d, void *array) {
+	memmove(rb2d->buffer[rb2d->writeIndex++ & rb2d->rows - 1],array,rb2d->cols * rb2d->eleSize);
+}
+
+void rb2dPop(RingBuffer2d *rb2d, void *array) {
+
+}
+
+void rb2dReadCol(RingBuffer2d *rb2d, void *array, int n) {
+
+}
+
+// Returns number of read/writeable rows
+size_t rb2dWriteCapacity(RingBuffer2d *rb2d) {
+
+}
+size_t rb2dReadCapacity(RingBuffer2d *rb2d) {
+
+}
+
+int rb2dFull(RingBuffer2d *rb2d) {
+
+}
+int rb2dEmpty(RingBuffer2d *rb2d) {
+
+}
+
+void rb2dPrintInt(RingBuffer2d *rb2d) {
+	printf("Printing all data as int.\n");
+	int i,j;
+	for (i = 0; i < (int)rb2d->rows; i++)
+	{
+		printf("\n");
+		void *bufferStart = rb2d->buffer[i];
+		for (j = 0; j < rb2d->cols; j++)
+		{
+			printf("%i,",*((int *)bufferStart + j));
+		}
+	}
+}
+void rb2dPrintFloat(RingBuffer2d *rb2d) {
+	printf("Printing all data as float\n");
+	int i,j;
+	for (i = 0; i < rb2d->rows; i++)
+	{
+		void *bufferStart = rb2d->buffer[i];
+		for (j = 0; j < rb2d->cols; j++)
+		{
+			printf("%f,",*((float *)bufferStart + j));
+		}
+		printf("\n");
+
+	}
+}
+
+int main(void) {
+    printf("\n::Testing each function::\n");
+    RingBuffer2d *rb2d = rb2dInit(10,14,sizeof(int));
+    rb2dPrintInt(rb2d);
+
+    int *emptyArr = calloc(rb2d->cols, sizeof(int));
+    int *onesArr = calloc(rb2d->cols, sizeof(int));
+    //memset(onesArr,1,rb2d->cols * sizeof(int));
+    int *arr1 = malloc(rb2d->cols * sizeof(int));
+    int i;
+    for (i = 0; i < rb2d->cols; i++)
+    {
+    	arr1[i] = i;
+    }
+    rb2dPush(rb2d, emptyArr);
+    rb2dPush(rb2d, arr1);
+    rb2dPush(rb2d, onesArr);
+    rb2dPrintInt(rb2d);
+}
---
+++ RingBuffer2d.h
@@ -0,0 +1,59 @@
+/*
+ 
+Copyright (c) 2012 R. Brett Jones
+
+Permission is hereby granted, free of charge, to any person obtaining a 
+copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the 
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included 
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE.
+
+*/
+
+/*
+  Struct for maintaining a rolling history of arrays. Rows is the history length,
+  cols is the length of the array.
+*/
+
+typedef struct RingBuffer2d {
+    void **buffer;
+    volatile unsigned int writeIndex; //row
+    volatile unsigned int readIndex; //row
+    int rows;
+    int cols;
+    size_t count;
+    size_t eleSize;
+} RingBuffer2d;
+
+// Rows will be rounded to next power of 2. Cols has no restrictions
+RingBuffer2d * rb2dInit(int numCol, int numRow, size_t elemsize);
+
+void rb2dPush(RingBuffer2d *rb2d, void *array);
+
+void rb2dPop(RingBuffer2d *rb2d, void *array);
+
+void rb2dReadCol(RingBuffer2d *rb2d, void *array, int n);
+
+// Returns number of read/writeable rows
+size_t rb2dWriteCapacity(RingBuffer2d *rb2d);
+size_t rb2dReadCapacity(RingBuffer2d *rb2d);
+
+int rb2dFull(RingBuffer2d *rb2d);
+int rb2dEmpty(RingBuffer2d *rb2d);
+
+void rb2dPrintInt(RingBuffer2d *rb2d);
+void rb2dPrintFloat(RingBuffer2d *rb2d);
+
+int roundNextPowerof2(int inVal);
